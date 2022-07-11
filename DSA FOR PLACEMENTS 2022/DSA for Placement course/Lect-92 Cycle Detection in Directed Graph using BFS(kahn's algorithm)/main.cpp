8e6db6bcc6feaac7061f8e7683e278d59830910e
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-07-11 23:41:11 +0530

    Lect-92 Cycle Detection in Directed Graph using BFS uploaded

---
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-92 Cycle Detection in Directed Graph using BFS(kahn's algorithm)/main.cpp
@@ -0,0 +1,89 @@
+//
+//  main.cpp
+//  Lect-92 Cycle Detection in Directed Graph using BFS(kahn's algorithm)
+//
+//  Created by Himanshu Bairwa on 11/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+/*
+ QUESTION: CYCLE DETECTION IN DIRECTED GRAPH USING KAHN'S ALGORITHM(USING BFS)
+ 
+ 
+ LINK-->  https://www.codingninjas.com/codestudio/problems/detect-cycle-in-a-directed-graph_1062626
+ 
+ 
+ CODE-->
+ 
+ #include<unordered_map>
+ #include<list>
+ #include<queue>
+
+ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+     
+     // create adj list
+     unordered_map<int,list<int> > adj;
+     for(int i = 0;i<edges.size();i++){
+         int u = edges[i].first - 1;
+         int v = edges[i].second - 1;
+         
+         adj[u].push_back(v);
+     }
+     
+     // find all indegrees of nodes
+     vector<int> indegree(n);
+     for(auto i: adj){
+         for(auto j: i.second){
+             indegree[j]++;
+             
+         }
+     }
+     
+     // 0 indegree valo ko queue ke andar push kardo
+     queue<int> q;
+     for(int i = 0;i<n;i++) {
+         if(indegree[i] == 0){
+             q.push(i);
+         }
+     }
+     
+     // do BFS
+
+     int cnt = 0;
+     
+     while(!q.empty()){
+         int front = q.front();
+         q.pop();
+         
+         // increase count
+             cnt++;
+         
+         //neighbour ke indegree ko update karo mean decrease karo
+         for(auto neighbour:adj[front]){
+             indegree[neighbour]--;
+             if(indegree[neighbour] == 0){
+                 q.push(neighbour);
+             }
+         }
+         
+     }
+     if(cnt == n){
+         return false;
+     }else{
+         return true;
+     }
+
+     
+     
+ }
+ 
+ 
+ */
