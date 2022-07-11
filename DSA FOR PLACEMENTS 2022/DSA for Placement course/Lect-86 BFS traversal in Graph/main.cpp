0405d2b2f66081e3810470f632335d86dbee9849
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-07-11 15:59:39 +0530

    Lect-86 uploaded

---
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-86 BFS traversal in Graph/main.cpp
@@ -0,0 +1,88 @@
+//
+//  main.cpp
+//  Lect-86 BFS traversal in Graph
+//
+//  Created by Himanshu Bairwa on 11/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+/*
+ QUESTION: BFS IN GRAPH
+ 
+ LINK--> https://www.codingninjas.com/codestudio/problems/bfs-in-graph_973002
+ 
+ CODE-->
+ 
+ #include<unordered_map>
+ #include<queue>
+ #include<list>
+ #include<set>
+
+ void prepareAdjList(unordered_map<int, set<int> > &adjList, vector<pair<int, int>> &edges) {
+     for(int i= 0;i<edges.size();i++){
+         int u = edges[i].first;
+         int v = edges[i].second;
+         
+         adjList[u].insert(v);
+         adjList[v].insert(u);
+     }
+ }
+
+ void bfs(unordered_map<int, set<int> > &adjList,unordered_map<int,bool> &visited,
+         vector<int> &ans,int node){
+     
+     queue<int> q;
+     q.push(node);
+     visited[node] = 1;
+     
+     while(!q.empty()){
+         int frontNode = q.front();
+         q.pop();
+         
+         //store frontNode in ans
+         ans.push_back(frontNode);
+         
+         // traverse all neighbours of frontNode
+         for(auto i : adjList[frontNode]){
+             if(!visited[i]){
+                 q.push(i);
+                 visited[i] = 1;
+             }
+         }
+     }
+ }
+
+
+
+
+ vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+ {
+     unordered_map<int, set<int> > adjList;
+     vector<int> ans;
+     unordered_map<int,bool> visited;
+     
+     prepareAdjList(adjList,edges);
+
+     
+     // travel all components of graph
+     for(int i = 0;i<vertex; i++){
+         if(!visited[i]){
+             bfs(adjList,visited,ans,i);
+         }
+     }
+     return ans;
+     
+ }
+ 
+ 
+ 
+ 
+ */
