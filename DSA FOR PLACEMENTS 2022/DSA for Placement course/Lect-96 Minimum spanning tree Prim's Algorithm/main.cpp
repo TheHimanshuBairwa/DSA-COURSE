96caf406ef51479db023b39a579ce3d4de2abbf1
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-07-12 16:15:23 +0530

    Lect-96 Prim's MST algorithm for graph uploaded

---
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-96 Minimum spanning tree Prim's Algorithm/main.cpp
@@ -0,0 +1,103 @@
+//
+//  main.cpp
+//  Lect-96 Minimum spanning tree Prim's Algorithm
+//
+//  Created by Himanshu Bairwa on 12/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+/*
+ QUESTION: PRIMS'S MST (V.V.VV.V.V.IMP)
+ 
+ 
+ LINK-->   https://www.codingninjas.com/codestudio/problems/prim-s-mst_1095633
+ 
+ 
+ CODE-->
+ 
+ #include<unordered_map>
+ #include<list>
+ #include<limits.h>
+
+
+
+ vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+ {
+     // create adj list
+     unordered_map<int,list<pair<int,int> > > adj;
+     for(int i=0; i<g.size();i++){
+         int u = g[i].first.first;
+         int v = g[i].first.second;
+         int w = g[i].second;
+         
+         adj[u].push_back(make_pair(v,w));
+         adj[v].push_back(make_pair(u,w));
+     }
+     
+     vector<int> key(n+1);
+     vector<bool> mst(n+1);
+     vector<int> parent(n+1);
+     
+     for(int i = 0; i<=n; i++){
+         key[i] = INT_MAX;
+         parent[i] = -1;
+         mst[i] = false;
+     }
+     
+     // lets start the algo
+     
+     key[1] = 0;
+     parent[1] = -1;
+     
+     for(int i =1; i<n; i++){
+         int mini = INT_MAX;
+         int u;
+         
+         // find min vali node
+         for(int v = 1; v<=n; v++){
+             if(mst[v] == false && key[v] < mini){
+                 u = v;
+                 mini = key[v];
+             }
+         }
+         
+         //mark min node as true
+         mst[u] = true;
+         
+         
+         // check its adjacent nodes
+         for(auto it: adj[u]) {
+             int v = it.first;
+             int w = it.second;
+             if(mst[v] == false && w < key[v]){
+                 parent[v] = u;
+                 key[v] = w;
+             }
+         }
+     }
+     
+    vector<pair<pair<int, int>,int> > result;
+     // loop ko 2 se chaleyenge bcoz 0 pe marking nai hai and 1 ka parent -1 hai to 2 se chaleyenge
+     for(int i = 2; i<=n ; i++){
+         result.push_back({{parent[i],i},key[i]});
+     }
+     
+         return result;
+     
+     
+     
+ }
+
+ 
+ 
+ 
+ 
+ */
