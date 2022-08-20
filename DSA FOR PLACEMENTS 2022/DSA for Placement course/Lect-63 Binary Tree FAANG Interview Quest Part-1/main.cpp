60da4b19e9a8085b312847b16ad244488f740134
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-08-20 11:19:15 +0530

    Diameter of Tree question updated with Striver Solution

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-63 Binary Tree FAANG Interview Quest Part-1/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-63 Binary Tree FAANG Interview Quest Part-1/main.cpp
@@ -131,6 +131,31 @@ int main() {
      }
  };
  
+ /*
+ APPROACH 3 BY ---> STRIVER
+ 
+ class Solution {
+public:
+    
+    int height(TreeNode* root, int &maxi){
+        if(root == NULL){
+            return 0;
+        }
+        
+        int lh = height(root->left,maxi);
+        int rh = height(root->right,maxi);
+         maxi = max(maxi,lh+rh);
+        return 1 + max(lh,rh);
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi = INT_MIN;
+        height(root, maxi);
+        return maxi;
+        
+    }
+};
+ 
  */
 
 /*
