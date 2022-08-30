23589cd3b733a1dacddcb8622e66152f060d6bb9
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-08-30 12:49:36 +0530

    Update main.cpp

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-88 Cycle detection in undirected graphs/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-88 Cycle detection in undirected graphs/main.cpp
@@ -100,5 +100,58 @@ int main(int argc, const char * argv[]) {
      return "No";
  }
 
+
+GFG QUESTION: DETECT CYCLE IN UNDIRECTED GRAPH USING DFS
+
+LINK:::: https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+
+Code: 
+
+
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+   
+   bool hasCycle(int node,int parent,vector<int>&vis,vector<int>adj[]){
+       vis[node]=true;
+       for(auto x:adj[node]){
+           if(!vis[x]){
+               if(hasCycle(x,node,vis,adj)){
+                   return true;
+               }
+           }
+           else if(vis[x]==true and x!=parent){
+               return true;
+           }
+       }
+       return false;
+   }
+   
+    bool isCycle(int V, vector<int> adj[]) {
+         // Code here
+       vector<int>vis(V,0);
+       for(int i=0;i<V;i++){
+           if(!vis[i]){
+               if(hasCycle(i,-1,vis,adj)){
+                   return true;
+               }
+           }
+       }
+       return false;
+   
+   
+      
+   
+    }
+};
+
+
+
+
+
+
+
+
  
  */
