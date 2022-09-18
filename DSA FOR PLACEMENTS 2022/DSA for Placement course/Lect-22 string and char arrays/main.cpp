289111f9f67fe922888f28244b619d01620deb48
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-09-18 12:36:22 +0530

    Q-7 remove spaces with some string updated

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-22 string and char arrays/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-22 string and char arrays/main.cpp
@@ -223,16 +223,37 @@ class Solution
     return finalAns;
 }
 
-
-
 };
+*/
+
+/*
+Question -7 Remove spaces by some sting
+
+link: https://www.codingninjas.com/codestudio/problems/replace-spaces_1172172
+
+
+code:  
+
+string replaceSpaces(string &str){
+	string temp = "";
+    for(int i =0; i< str.length(); i++){
+
+    if(str[i] == ' '){
+
+        temp.push_back('@');
+        temp.push_back('4');
+        temp.push_back('0');
+    }else
+        temp.push_back( str[i]);
+    
+    }
+    return temp;
+}
+
 
 
 
 */
-
-
-
 
 
 
