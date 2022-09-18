d70a086846a360990d6c38aa836a5982b829b0eb
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-09-18 14:51:27 +0530

    Q-9 Permutation of string updated

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-22 string and char arrays/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-22 string and char arrays/main.cpp
@@ -253,7 +253,7 @@ string replaceSpaces(string &str){
 
 
 /*
-Question : Remove all occurrence of substring
+Question 8 : Remove all occurrence of substring
 
 link: https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
 
@@ -273,6 +273,74 @@ public:
 };
 */
 
+/*
+Question 9 : Permutation in String
+link: https://leetcode.com/problems/permutation-in-string/
+
+code: 
+
+class Solution {
+private: 
+    bool checkequal(int a[26],int b[26]){
+        for(int i = 0; i < 26 ; i++ ) {
+            if(a[i] != b[i]){
+                return 0;
+            }
+            
+        }return 1;
+    }
+    
+    public:
+    bool checkInclusion(string s1, string s2) {
+        
+        // character count array
+        int count1[26] = {0};
+        for(int i = 0; i< s1.length(); i++) {
+            int index = s1[i] - 'a';
+            count1[index]++;
+        }
+        
+        //traverse in s2 string with window size s1 length and compare
+        
+        int i = 0;
+        int windowsize = s1.length();
+        int count2[26] = {0};
+
+        //running for first window
+        while(i < windowsize && i < s2.length()){
+            int index = s2[i] - 'a';
+            count2[index]++;
+            i++;
+            
+        }
+        if (checkequal(count1,count2)) {
+            return 1;
+        }
+        
+        //aage window process karo
+        
+        while(i < s2.length()) {
+            char newchar = s2[i];
+            int index = newchar - 'a';
+            count2[index]++;
+            
+            char oldchar = s2[i - windowsize];
+            index = oldchar - 'a';
+            count2[index]--;
+            i++;
+            
+            if (checkequal(count1,count2)) {
+            return 1;
+                
+        }
+        
+
+        }
+        return 0;
+    }
+};
+
+*/
 
 
 
