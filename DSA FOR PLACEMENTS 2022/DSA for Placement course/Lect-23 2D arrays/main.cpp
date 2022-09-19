be20ac234001b1d773c64dfb98017a2f6d26d87b
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-09-19 12:27:35 +0530

    Q-5 SEARCH IN 2D MATRIX TYPE 2  UPDATED

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-23 2D arrays/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-23 2D arrays/main.cpp
@@ -252,7 +252,9 @@ public:
 
 /*
 
-QUESTION 4: SEARCH IN 2D MATRIX (BINARY SEARCH IN MATRIX)
+QUESTION 4: SEARCH IN 2D MATRIX (BINARY SEARCH IN MATRIX) 
+                           (ONLY ROW SORTED AND The first integer of each row is greater than the last integer of the previous roW)
+ 
 
 LINK: https://leetcode.com/problems/search-a-2d-matrix/
 
@@ -293,7 +295,43 @@ public:
 */
 
 
+/*
+QUESTION 5: SEARCH IN SORTED MATRIX 2 (ROW AND COLOUMN BOTH ARE SORTED)
 
+LINK: https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+CODE:  
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int rowindex = 0;
+        int colindex = col-1;
+        
+        while(rowindex < row && colindex >= 0){
+             int element = matrix[rowindex][colindex];
+            
+            if(element == target){
+                return 1;
+            }else if( element < target){
+                rowindex++;
+            }else{
+                colindex--;
+            }
+            
+            
+            
+        }  
+        return 0;
+        
+    }
+};
+
+*/
 
 
 
