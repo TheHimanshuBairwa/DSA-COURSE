69d540e783b522e895f06c94dd6ac224dda7c503
Himanshu N Bairwa <88283732+TheHimanshuBairwa@users.noreply.github.com>
2022-09-19 10:58:49 +0530

    Q-1 spiral print updated

--- DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-23 2D arrays/main.cpp
+++ DSA FOR PLACEMENTS 2022/DSA for Placement course/Lect-23 2D arrays/main.cpp
@@ -132,3 +132,38 @@ int main() {
     cout<<3%4<<endl;
     return 0;
 }
+
+/*
+QUESTION 1 : PRINT SPIRAL
+
+LINK: https://www.codingninjas.com/codestudio/problems/print-like-a-wave_893268
+
+CODE: 
+
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+     vector<int> ans;
+    for(int col = 0; col <mCols ; col++){
+        
+        //check for col even or odd
+        if(col&1){
+            
+            for(int row = nRows-1; row>=0; row--){
+
+                ans.push_back(arr[row][col]);
+            }
+            
+        }else {
+            for(int row = 0 ; row <   nRows ; row++){
+
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
+
+*/
+
+
+
